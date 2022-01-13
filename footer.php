6c4cec0d79776202d30fa63bce38301597b4a4ed
ArzilRiyaj <64822047+ArzilRiyaj@users.noreply.github.com>
2022-01-13 17:13:39 +0530

    first

---
+++ footer.php
@@ -0,0 +1,111 @@
+
+<!-- Footer -->
+
+
+<footer class="text-center text-lg-start parallax " style=" color: white; background-image: url(assets/images/body-bg.jpg);  ">
+  <!-- Section: Social media -->
+  <section
+    class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+  >
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block mt-4">
+      <span>Get connected with us on social networks:</span>
+    </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <div>
+      <a href="https://www.facebook.com/Bobos.bbp" class="me-4 text-reset">
+        <i class="fab fa-facebook"></i>
+      </a>
+      <!-- <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a> -->
+    
+      <a href="https://www.instagram.com/bobos.bbp/" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+     
+
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-4 col-xl-3 mx-auto mb-4 ">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+          <img src="assets/images/b.png" width="25" alt="Logo"> Bobos Biriyani Pot &reg; &trade;
+          </h6>
+          <p>
+          If You Are Among Those Who Find Pleasure In Home Cooked Food, You Are In The Right Place. Preparation Of All Cuisines With Different Condiments And Finest Ingredients With A Twist Of Sri Lankan Spices Will Give Your Tastebuds A Meal To Remember!
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-4 col-xl-4 mb-4 justify-content-end">
+          <div class="mx-5 justify-content-center" style="padding-left: 20px;">
+          <img src="assets/images/Untitled design (6).png" width="200" alt="Logo">
+          </div>
+        
+        </div> 
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Contact
+          </h6>
+
+          <p><i >   <img src="assets/images/b.png" width="25" alt="Logo"></i> BOBOS BIRIYANI (PVT)Ltd. </p>
+          <p><i class="fas fa-home me-3"></i> Devananda Road Piliyandala 10300 Piliyandala, Sri Lanka</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            info@bobosbiriyanipot.lk
+          </p>
+          <p><i class="fas fa-phone me-3"></i>+94 77 715 4945</p>
+          <!-- <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p> -->
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2021 Copyright:
+    <a class="text-reset fw-bold" href="https://www.instagram.com/shadzil_digi/">Devoloped By Shadzil - The social media agency</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+    <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+ 
+   
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+   
+    <script src="assets/js/animate.js"></script>
+  
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+ 
+  
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,47 @@
+<nav class="navbarcustome sticky-top bg-transparent navbar-expand-lg navbar-light bg-light px-2 mb-3" style="background-image: url('assets/images/sketch-header.png'); background-repeat: no-repeat; background-size: 100% 105%;">
+  
+  <a class="navbar-brand" href="#"><img src="assets/images/Untitled design (6).png" class="ms-4" width="100" alt="Logo"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+    
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#menu">Menu</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#abt">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="#contact">Contact Us</a>
+      </li>
+      <li class="nav-item">
+     
+     <a  class="nav-link " href="https://wa.me/+94777154945" target="_blank"  > <button class="btn btn-outline my-2 my-sm-0" type="button">Order Now</button> </a>
+
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link " href="#"></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="#"></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="#"></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="#"></a>
+      </li>
+      <!-- <li class="nav-item">
+        <a class="nav-link " href="#"></a>
+      </li> -->
+    </ul>
+    
+  </div>
+</nav>
